5338c23ce7cb714fd95af92dfcaab5b80429b4bb
MauBao <ngomaubaodhcn1b@gmail.com>
2017-04-20 10:51:35 +0700

    Chuong trinh in so le tu 1->n

---
+++ macABC.sh
@@ -0,0 +1,14 @@
+﻿#file maxABC.sh
+#./maxABC.sh 2 13 7
+#out SLN la: 13
+
+m=$1
+if [$m-lt $2]
+  then
+    m=$2
+  fi
+if [$m-lt $3]
+  then
+  m=$3
+ fi
+  echo "SLN la: $m"
---
+++ sole.sh
@@ -0,0 +1,10 @@
+﻿# In ra bang cuu chuong
+#
+
+n=$1 
+i=1
+  while ($i -le 10)
+    do
+	echo "$n*$i = expr $i \* $n"
+	  i=(expr $i +1)
+	done
